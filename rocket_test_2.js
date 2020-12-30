ba4b282d877a50d8e744744483f3dd0ecc25a185
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2020-12-30 19:26:31 +0530

    Add files via upload

--- rocket_test_2.js
+++ rocket_test_2.js
@@ -63,8 +63,105 @@ function project(){
     //rocket variables
 
 
-    var Mi=$('#Massi').val();         
-    var Mp
+    var Mi=$('#massi').val();         
+    var Mp=$('#massp').val();         
+    var Mf=$('#massf').val();         
     var Pexh=$('#pressureexh').val();    //exhaust pressure
+    var Aexh=$('#areaexh').val();         
+    var dmdt=$('#dmdt').val();     
+    var dmdtms=dmdt/100;
+
+    Mnet=Mi +Mp + Mf;
+    var Patm=1;
+
+    var rocket=Bodies.rectangle(renderwidth/2,renderheight-20,10,40);
+
+
+
+
+
+
+    World.add(engine.world,[rocket]);
+
+
+
+    //start loop
+
+    var changeforce= setInterval(function(){
+        if (Mf>0){
+
+            Mnet=Mi +Mp + Mf;
+            Matter.Body.setMass(rocket,Mnet);
+            Fexh=(Pexh-Patm)*Aexh;
+            Mf=Mf-dmdtms;
+            Fnet=Fexh/50;
+
+
+        }
+        else if(Mf<=0){
+            Mf=0;
+            Fnet=0;
+        }
+
+    },10);
+
     
+    Matter.Events.on(engine,'beforeUpdate',function(event){
+        Body.applyForce(rocket,{
+            x:rocket.position.x,
+            y:rocket.position.y+20
+        },{
+            x:0,
+            y:-Fnet
+        });
+
+    });
+
+    var ydata=[]; 
+    //graph 
+    var options={
+        chart:{
+            type:'line',
+            id:'realtime',
+            height:400,
+            animations:{
+                enabled:true,
+                easing:'linear',
+                dynamicAnimation:{
+                    speed:1
+                }
+            }
+        }
+        ,
+        stroke:{
+            curve:'straight'
+        },
+        series:[{
+            data:ydata
+        }],
+        xaxis:{
+            type:'numeric'
+        }
+    }
+
+    var chartvelocity=new ApexCharts(document.querySelector("#velchart"),options);
+
+    chartvelocity.render();
+    val_x=0;
+
+    setInterval(function(){
+        vlx=val_x/100;
+        let vl=rocket.velocity.y;
+        vl=-vl;
+        yvl_new=[vlx,vl];
+        ydata.push(yvl_new);
+        val_x++;
+
+        chartvelocity.updateSeries([{
+            data:ydata
+        }])
+
+    },10);
+
+
 }
